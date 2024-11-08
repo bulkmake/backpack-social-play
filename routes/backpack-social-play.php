42b96a9a4d42c9759109976c023a43ffb7e6837c
Karan Datwani <karandatwani92@gmail.com>
2024-11-08 20:02:48 +0530

    crud skeleton

--- routes/backpack-social-play.php
+++ routes/backpack-social-play.php
@@ -1,38 +1,12 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Bulkmake\BackpackSocialPlay Routes
-|--------------------------------------------------------------------------
-|
-| This file is where you may define all of the routes that are
-| handled by the Bulkmake\BackpackSocialPlay package.
-|
-*/
-
-/**
- * User Routes
- */
-
-// Route::group([
-//     'middleware'=> array_merge(
-//     	(array) config('backpack.base.web_middleware', 'web'),
-//     ),
-// ], function() {
-//     Route::get('something/action', \Bulkmake\BackpackSocialPlay\Http\Controllers\SomethingController::actionName());
-// });
-
-
-/**
- * Admin Routes
- */
-
-// Route::group([
-//     'prefix' => config('backpack.base.route_prefix', 'admin'),
-//     'middleware' => array_merge(
-//         (array) config('backpack.base.web_middleware', 'web'),
-//         (array) config('backpack.base.middleware_key', 'admin')
-//     ),
-// ], function () {
-//     Route::crud('some-entity-name', \Bulkmake\BackpackSocialPlay\Http\Controllers\Admin\EntityNameCrudController::class);
-// });
+Route::group([
+    'prefix' => config('backpack.base.route_prefix', 'admin'),
+    'middleware' => array_merge(
+        (array) config('backpack.base.web_middleware', 'web'),
+        (array) config('backpack.base.middleware_key', 'admin')
+    ),
+    'namespace' => 'Bulkmake\BackpackSocialPlay\Http\Controllers\Admin',
+], function () { // custom admin routes
+    Route::crud('social-post', 'SocialPostCrudController');
+});
